45c59654e46683a0545ea51f8797ec43e2864365
tusharnankani <tusharnankani3@gmail.com>
2023-05-31 21:53:11 +0530

    chore: setup static graphql queries

--- src/App.js
+++ src/App.js
@@ -1,84 +1,93 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import { Modal } from './containers'
+import { Modal } from "./containers";
+import { useQuery } from "@apollo/client";
+import { POKEMON_QUERY, MOVES_QUERY } from "./graphql/get-pokemon";
 
 const KEY_CODE = {
-  CTRL: "17",
-  CTRL_K: "1775",
-  ESC: "27",
+	CTRL: "17",
+	CTRL_K: "1775",
+	ESC: "27"
 };
 
 function App() {
-  let globalString = "";
-  const homeRef = useRef(null);
-  const [openModal, setOpenModal] = useState(false);
-  
-  const resetGlobalString = () => {
-    globalString = "";
-    focusHomeComponent();
-  };
 
-  /**
-   * handleKeyDown function
-   * To detect keydown events and identify `CTRL K` or `Esc`
-  */
- const handleKeyDown = (event) => {
-    let keyCode = event.keyCode || event.which;
-    globalString += keyCode;
+	const { data: { pokemons1 = [] } = {}, loading, error} = useQuery(POKEMON_QUERY);
+	const { data: { pokemons2 = [] } = {} } = useQuery(MOVES_QUERY);
 
-    // detect CTRL + K;
-    if (globalString.slice(-4) === KEY_CODE.CTRL_K) {
-      // to override CTRL + K of the browser;
-      event.preventDefault();
+	let globalString = "";
+	const homeRef = useRef(null);
+	const [openModal, setOpenModal] = useState(false);
 
-      // change state here;
-      setOpenModal((prevState) => !prevState);
+	const resetGlobalString = () => {
+		globalString = "";
+		focusHomeComponent();
+	};
 
-      // reset string to avoid overflow
-      resetGlobalString();
+	/**
+	 * handleKeyDown function
+	 * To detect keydown events and identify `CTRL K` or `Esc`
+	 */
+	const handleKeyDown = (event) => {
+		let keyCode = event.keyCode || event.which;
+		globalString += keyCode;
 
-    } else {
-      let lastTwoKeys = globalString.slice(-2);
+		// detect CTRL + K;
+		if (globalString.slice(-4) === KEY_CODE.CTRL_K) {
+      
+			// to override CTRL + K of the browser;
+			event.preventDefault();
 
-      // if Esc; simply close the modal 
-      if (lastTwoKeys === KEY_CODE.ESC) {
-        event.preventDefault();
-        setOpenModal(false);
-        resetGlobalString();
-      }
-      else if (lastTwoKeys !== KEY_CODE.CTRL && lastTwoKeys !== KEY_CODE.ESC) {
-        resetGlobalString();
-      }
-    }
-  };
+			// change state here;
+			setOpenModal((prevState) => !prevState);
 
-  /**
-   * Function to gain focus when component loses focus;
-   * Such that Ctrl K is successfully activated;
-   */
-  const focusHomeComponent = () => {
-    homeRef.current.focus();
-  };
+			// reset string to avoid overflow
+			resetGlobalString();
 
-  useEffect(() => {
-    // Function to focus when the component renders;
-    focusHomeComponent();
-  }, []);
+		} else {
+			let lastTwoKeys = globalString.slice(-2);
 
-  return (
-    <div
-      className="home"
-      ref={homeRef}
-      onKeyDown={handleKeyDown}
-      onBlur={focusHomeComponent}
-      tabIndex="0"
-    >
-      Press CTRL K to get started.<br/>
-      Modal Open: {openModal ? "True" : "False"}
-      <Modal/>
-    </div>
-  );
+			// if Esc; simply close the modal
+			if (lastTwoKeys === KEY_CODE.ESC) {
+				event.preventDefault();
+				setOpenModal(false);
+				resetGlobalString();
+			} else if (
+				lastTwoKeys !== KEY_CODE.CTRL &&
+				lastTwoKeys !== KEY_CODE.ESC
+			) {
+				resetGlobalString();
+			}
+		}
+	};
+
+	/**
+	 * Function to gain focus when component loses focus;
+	 * Such that Ctrl K is successfully activated;
+	 */
+	const focusHomeComponent = () => {
+		homeRef.current.focus();
+	};
+
+	useEffect(() => {
+		// Function to focus when the component renders;
+		focusHomeComponent();
+	}, []);
+
+	return (
+		<div
+			className="home"
+			ref={homeRef}
+			onKeyDown={handleKeyDown}
+			onBlur={focusHomeComponent}
+			tabIndex="0"
+		>
+			Press CTRL K to get started. Modal Open:{" "}
+			{openModal ? "True" : "False"}
+			<Modal />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/graphql/get-pokemon.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+export const POKEMON_QUERY = gql`
+query PokemonQuery {
+    pokemons: pokemon_v2_pokemonspecies(
+        where: {
+          pokemon_v2_generation: { name: { _eq: "generation-iii" } }
+          pokemon_v2_pokemoncolor: { name: { _eq: "green" } }
+          pokemon_v2_pokemonhabitat: {
+            pokemon_v2_pokemonhabitatnames: { name: { _in: ["grassland", "mountain", "water's edge"] } }
+          }
+        }
+        order_by: { id: asc }
+      ) {
+        name
+        id
+        base_happiness
+      }
+  }
+`
+
+export const MOVES_QUERY = `
+query MovesQuery {
+    pokemon_v2_move(where: { pp: { _gt: 20 }, pokemon_v2_movedamageclass: { name: { _eq: "physical" } } }) {
+      name
+      damage_class: pokemon_v2_movedamageclass {
+        name
+      }
+      pp
+      priority
+    }
+  }
+`
