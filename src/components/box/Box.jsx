ef95bee86928b3b4468409d8ebcb7de34764ee72
Tushar Nankani <tusharnankani3@gmail.com>
2023-05-31 12:54:07 +0000

    chore: identify box types, modify box to btn

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import './box.css'
 import arrow from './../../assets/right-arrow.svg';
 
+/**
+ * Types of Box
+ * 1. Filter
+ *      Three states
+ *      - Inactive 
+ *      - Focus (Hover) 
+ *      - Active (Click)
+ *      View
+ *      - .radio <hide> 
+ *      - .text  
+ *      - .cnt <?conditional>
+ *      - .right-arrow  
+ * 2. Option
+ *      Three states
+ *      - Inactive 
+ *      - Focus (Hover) 
+ *      - Active (Click)
+ *      View
+ *      - .radio [radio/check]
+ *      - .text  
+ *      - .cnt <hide>
+ *      - .right-arrow <hide>
+ *      - Radio
+ *      - Checkbox
+ */
+
 function Box() {
     return ( 
-        <div className='box'>
+        <button className='box'>
             <div className="first">
                 <span className="radio"></span>
                 <span className="text">Pokemons</span>
@@ -13,7 +39,7 @@ function Box() {
                 <span className="cnt">4</span>
                 <img src={arrow} alt="right-arrow" />
             </div>
-        </div> 
+        </button> 
     );
 }
 
