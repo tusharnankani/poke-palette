39ce0cda342002e68d1d748804e8da023bb59980
Tushar Nankani <tusharnankani3@gmail.com>
2023-05-31 13:23:27 +0000

    chore: setup generalized conditional box component

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -1,46 +1,61 @@
-import React from 'react';
-import './box.css'
-import arrow from './../../assets/right-arrow.svg';
+import React from "react";
+import "./box.css";
+import arrow from "./../../assets/right-arrow.svg";
 
 /**
  * Types of Box
  * 1. Filter
  *      Three states
- *      - Inactive 
- *      - Focus (Hover) 
+ *      - Inactive
+ *      - Focus (Hover)
  *      - Active (Click)
  *      View
- *      - .radio <hide> 
- *      - .text  
+ *      - .radio <hide>
+ *      - .text
  *      - .cnt <?conditional>
- *      - .right-arrow  
+ *      - .right-arrow
  * 2. Option
  *      Three states
- *      - Inactive 
- *      - Focus (Hover) 
+ *      - Inactive
+ *      - Focus (Hover)
  *      - Active (Click)
  *      View
  *      - .radio [radio/check]
- *      - .text  
+ *      - .text
  *      - .cnt <hide>
  *      - .right-arrow <hide>
  *      - Radio
  *      - Checkbox
  */
 
-function Box() {
-    return ( 
-        <button className='box'>
-            <div className="first">
-                <span className="radio"></span>
-                <span className="text">Pokemons</span>
-            </div>
-            <div className="second">
-                <span className="cnt">4</span>
-                <img src={arrow} alt="right-arrow" />
-            </div>
-        </button> 
-    );
+function Box({
+	selectBool = false,
+	selectType = "",
+	text = "Default Box",
+	cnt = 0,
+	arrowBool = true
+}) {
+	return (
+		<button className="box">
+			<div className="first">
+				{/* Checkbox/Radio */}
+                {console.log(selectBool)}
+				{selectBool &&
+					(<span className={`select ${selectType}`}></span>)}
+
+				{/* Checkbox/Radio */}
+				<span className="text">{text}</span>
+			</div>
+
+			<div className="second">
+				{/* Filter Count */}
+				{cnt > 0 && (<span className="cnt">{cnt}</span>)}
+
+				{/* Right Arrow */}
+				{arrowBool && (<img src={arrow} alt="right-arrow" />)}
+			</div>
+		</button>
+	);
 }
 
 export default Box;
