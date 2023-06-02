1f9e0ae807cb4eb9e4bdc091aa2c510c48d740cc
tusharnankani <tusharnankani3@gmail.com>
2023-06-02 14:40:12 +0530

    restruct: make graphql queries dynamic

--- src/graphql/get-pokemon.js
+++ src/graphql/get-pokemon.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 
+
+
+/**
+ * POKEMON_QUERY 
+ * - Dynamic query for Pokemon filters like:
+ *    Generation - String
+ *    Color - String
+ *    Habitat - [String]
+ *
+ * Expected variables of the format:
+  
+  {
+    "generation": "generation-i",
+    "pokemonColor": "red",
+    "pokemonHabitatNames": ["grassland"],
+  }
+
+*/
+
 export const POKEMON_QUERY = gql`
-query PokemonQuery {
+query PokemonQuery(
+  $generation: String,
+  $pokemonColor: String,
+  $pokemonHabitatNames: [String!],
+) {
     pokemons: pokemon_v2_pokemonspecies(
         where: {
-          pokemon_v2_generation: { name: { _eq: "generation-iii" } }
-          pokemon_v2_pokemoncolor: { name: { _eq: "green" } }
+          pokemon_v2_generation: { name: { _eq: $generation } }
+          pokemon_v2_pokemoncolor: { name: { _eq: $pokemonColor} }
           pokemon_v2_pokemonhabitat: {
-            pokemon_v2_pokemonhabitatnames: { name: { _in: ["grassland", "mountain", "water's edge"] } }
+            pokemon_v2_pokemonhabitatnames: { name: { _in: $pokemonHabitatNames } }
           }
         }
         order_by: { id: asc }
@@ -20,9 +43,33 @@ query PokemonQuery {
   }
 `
 
+
+/**
+ * MOVES_QUERY 
+ * - Dynamic query for Moves filters like:
+ *    Power Points - Int
+ *    Move Class - String
+ *
+ * Expected variables of the format:
+  
+  {
+    "powerPoints": 20,
+    "moveClass": "physical",
+  }
+
+*/
+
 export const MOVES_QUERY = gql`
-query MovesQuery {
-    pokemon_v2_move(where: { pp: { _gt: 20 }, pokemon_v2_movedamageclass: { name: { _eq: "physical" } } }) {
+query MovesQuery(
+  $powerPoints: Int,
+  $moveClass: String,
+) {
+    pokemon_v2_move(
+      where: { 
+        pp: { _gt: $powerPoints }, 
+        pokemon_v2_movedamageclass: { name: { _eq: $moveClass } } 
+      }
+    ) {
       name
       damage_class: pokemon_v2_movedamageclass {
         name
